a3a9ed3117ac7aeca7c0d84658d6b2f8deb4a772
clemmlec <clement.canale@gmail.com>
2022-09-19 15:06:47 +0200

    feat: sneakers methos post

--- src/Controller/SneakersController.php
+++ src/Controller/SneakersController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Sneakers;
+use App\Repository\SneakersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SneakersController extends AbstractController
 {
-    #[Route('/sneakers', name: 'app_sneakers')]
+    #[Route('/sneakers', name: 'app_sneakers', methods:['GET'])]
     public function index(): JsonResponse
     {
         return $this->json([
@@ -16,4 +21,25 @@ class SneakersController extends AbstractController
             'path' => 'src/Controller/SneakersController.php',
         ]);
     }
+
+    #[Route('/sneakers', name: 'app_sneakers_create', methods: ['POST'])]
+    public function create(Request $request, SerializerInterface $serializer,  SneakersRepository $em): JsonResponse
+    {
+        
+        $sneakers = $serializer->deserialize($request->getContent(), Sneakers::class, 'json');
+        
+        if($em->add($sneakers, true)){
+            return $this->json([
+                'message' => 'sneakers créer avec succes '
+            ]);
+        }else{
+            return $this->json([
+                'message' => 'sneakers non créer un probleme est survenu'
+            ]);
+        }
+        
+
+       
+    }
+
 }
